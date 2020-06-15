973b05f80705194ef6f3bd040bf569332dd4bb7e
TheManohar <manohar.sitapati@gmail.com>
2020-06-15 15:51:10 +0200

    add db_dump script

--- db_dump.py
+++ db_dump.py
@@ -5,25 +5,32 @@ import mysql.connector
 from load import connect_base
 from CN_config import mysql_u, mysql_p
 
-def show_tables(mydb):
+def show_tables(mydb, db_name):
     import mysql.connector
     mycursor = mydb.cursor()
+    mycursor.execute(f'USE {db_name}')
     mycursor.execute("SHOW TABLES")
-    print('Tables in current DB:')
-    for x in mycursor:
-        print(x)
-    return mycursor
+    return [x for x in mycursor]
 
-def select_data(mydb):
+def show_columns(mydb, db_name, table_name):
     mycursor = mydb.cursor()
-    mycursor.execute('SELECT * FROM articles')
+    mycursor.execute(f'USE {db_name}')
+    mycursor.execute(f'SHOW COLUMNS FROM {table_name}')
+    return [x[0] for x in mycursor]
+
+def select_data(mydb, db_name, table_name):
+    mycursor = mydb.cursor()
+    mycursor.execute(f'USE {db_name}')
+    mycursor.execute(f'SHOW COLUMNS FROM {table_name}')
+    column_names = [x[0] for x in mycursor]
+    mycursor.execute(f'SELECT * FROM {table_name}')
     myresult = mycursor.fetchall()
-    return pd.DataFrame(myresult, columns=['publishedAt', 'source', 'source_id', 'author', 'title', 'description', 'content', 'url'])
+    return pd.DataFrame(myresult, columns=column_names)
 
 def save_df(df, df_name):
-    return df.to_pickle(f'{datetime.datetime.now().strftime("%y%m%d-%H%M%S")}-{df_name}.pkl')
+    return df.to_pickle(f'df_data/{df_name}.pkl')
 
 if __name__ == "__main__":
     mydb = connect_base(mysql_u, mysql_p)
-    df = select_data(mydb)
-    save_df(df)
+    df = select_data(mydb, 'crypto_news', 'articles')
+    save_df(df, 'df_articles')
