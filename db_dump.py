e115ee57fea4032a4b09542a564b1cf0dce3282e
TheManohar <manohar.sitapati@gmail.com>
2020-06-15 14:21:31 +0000

    db_dump fix

--- db_dump.py
+++ db_dump.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3.7
 
-import datetime
-import mysql.connector
+import pandas as pd
 from load import connect_base
 from CN_config import mysql_u, mysql_p
 
 def show_tables(mydb, db_name):
-    import mysql.connector
     mycursor = mydb.cursor()
     mycursor.execute(f'USE {db_name}')
     mycursor.execute("SHOW TABLES")
@@ -25,12 +23,15 @@ def select_data(mydb, db_name, table_name):
     column_names = [x[0] for x in mycursor]
     mycursor.execute(f'SELECT * FROM {table_name}')
     myresult = mycursor.fetchall()
-    return pd.DataFrame(myresult, columns=column_names)
+    df = pd.DataFrame(myresult, columns=column_names)
+    df.name = f'df_{table_name}'
+    return df
 
-def save_df(df, df_name):
-    return df.to_pickle(f'df_data/{df_name}.pkl')
+def save_df(df):
+    return df.to_pickle(f'df_data/{df.name}.pkl')
 
 if __name__ == "__main__":
     mydb = connect_base(mysql_u, mysql_p)
-    df = select_data(mydb, 'crypto_news', 'articles')
-    save_df(df, 'df_articles')
+    df = select_data(mydb, 'msitapati$crypto_news', 'articles')
+    save_df(df)
+    print(f'CREATED: {df.name}.plk')
