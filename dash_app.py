f1e9994428286f1b219923ba38c4af6d80b8af5b
TheManohar <manohar.sitapati@gmail.com>
2020-06-17 17:08:23 +0000

    update dashboard

--- dash_app.py
+++ dash_app.py
@@ -17,13 +17,20 @@ colors = {
     'bitcoin': '#d4d12c',
     'ethereum': '#4b5b75'}
 
+initial_range = [df.index[13], df.index[0]+pd.DateOffset(1)]
+
 app.layout = html.Div([
     html.H1('CryptoNews',
             style={'textAlign': 'center'}),
 
-    html.Div('An API based on Flask. It collects and displays the latest news about cryptocurrency from various news sources.',
-             style={'textAlign': 'center'}),
+    dcc.Markdown('''
+                    A project based on [Flask](https://stackshare.io/flask) and [Dash](https://plotly.com/dash). \n
+                    The **ETL** collects the latest news about crypto from various news sources to a [MySQL](https://www.mysql.com/) database. \n
+                    **Data Analysis** performs a sentiment analysis on headlines and description of articles, by using [vaderSentiment](https://pypi.org/project/vaderSentiment/). \n
+                    Finally, this **Dashboard** displays the average sentiment score for the collected articles and how many were evaluated.
 
+                '''),
+html.Div([
     dcc.Graph(id='score-lines',
         figure = {'data':[
             go.Scatter(
@@ -31,43 +38,39 @@ app.layout = html.Div([
                     y=df['CRY_score'],
                     name = 'Crypto',
                     fill='tozeroy',
-                    mode = 'lines+markers',
+                    mode = 'lines',
                     marker=dict(color=colors['crypto'])),
             go.Scatter(
                     x=df.index,
                     y=df['BTC_score'],
                     name = 'Bitcoin',
                     fill='tozeroy',
-                    mode = 'lines+markers',
+                    mode = 'lines',
                     marker=dict(color=colors['bitcoin'])),
             go.Scatter(
                     x=df.index,
                     y=df['ETH_score'],
                     name = 'Ethereum',
                     fill='tozeroy',
-                    mode = 'lines+markers',
+                    mode = 'lines',
                     marker=dict(color=colors['ethereum']))],
                 'layout': go.Layout(
                 title = 'Daily Sentiment Score',
-                yaxis = {'title': 'Average Sentiment Score'},
-                xaxis=dict(
-                rangeselector=dict(
-                    buttons=list([
-                        dict(count=8,
-                             label="1w",
-                             step="day",
-                             stepmode="backward"),
-                        dict(count=1,
-                             label="1m",
-                             step="month",
-                             stepmode="backward"),
-                        dict(step="all")
-                    ])
-                ),
-                rangeslider=dict(
-                    visible=False
-                ),
-                type="date"))}),
+                yaxis = dict(title= 'Average Sentiment Score',
+                             tickformat=".2%"),
+                xaxis = dict(rangeselector=dict(buttons=list([dict(count=7,
+                                                                 label="1w",
+                                                                 step="day",
+                                                                 stepmode="backward"),
+                                                            dict(count=1,
+                                                                 label="1m",
+                                                                 step="month",
+                                                                 stepmode="backward"),
+                                                            dict(step="all")])),
+                            rangeslider = dict(visible=False),
+                            tickformat = '%a %d-%m',
+                            type = "date",
+                            range=initial_range))}),
 
     dcc.Graph(id='count-bars',
         figure={'data':[
@@ -90,22 +93,22 @@ app.layout = html.Div([
                 title = 'Daily Articles Count',
                 yaxis = {'title': 'Number of mentions in headline'},
                 xaxis=dict(
-                rangeselector=dict(
-                    buttons=list([
-                        dict(count=8,
-                             label="1w",
-                             step="day",
-                             stepmode="backward"),
-                        dict(count=1,
-                             label="1m",
-                             step="month",
-                             stepmode="backward"),
-                        dict(step="all")
-                    ])
-                ),
-                rangeslider=dict(
-                    visible=False
-                ),
-                type="date"))}),
-
-])
+                rangeselector=dict(buttons=list([dict(count=7,
+                                                      label="1w",
+                                                      step="day",
+                                                      stepmode="backward"),
+                                                 dict(count=1,
+                                                      label="1m",
+                                                      step="month",
+                                                      stepmode="backward"),
+                                                 dict(step="all")])),
+                                rangeslider=dict(visible=False),
+                                tickformat = '%a %d-%m',
+                                type="date",
+                                range=initial_range))}),
+]),
+html.Div([
+    dcc.Markdown('''
+                    A project by [Manohar Sitapati](https://www.linkedin.com/in/manoharsitapati/), find the whole original code in [GitHub](https://github.com/TheManohar/CryptoNews)
+                '''),
+])])
